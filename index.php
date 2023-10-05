18a8912ea20dd3c683d62834f0fc7ea778117115
harikakolipaka22k <146998058+harikakolipaka22k@users.noreply.github.com>
2023-10-04 17:36:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h2>PHP is Fun, This is saksham site!</h2>";
+echo "<h2>PHP is Fun, This is Harika's site!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
