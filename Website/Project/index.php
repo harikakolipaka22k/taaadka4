6a38c6873ec002162ff384a57cc319f258fafd17
Harika Kolipaka <harika@Harikas-MacBook-Pro.local>
2023-10-11 16:01:56 -0700

    website

---
+++ Website/Project/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php require "head.php" ?>
+    <title>Enterprise Software Platforms</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand">CMPE - 272</a>
+            <a class="navbar-brand">Prof Richard Sinn</a>
+            <a class="navbar-brand">Krishna Gupta - 015721199</a>
+        </div>
+    </nav>
+
+
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-4 my-2">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Homework 1</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Sept 18, 2021</h6>
+                        <p class="card-text">In this homework, you have to setup your own web prorgramming environment.</p>
+                        <a href="homework1/" class="card-link">Checkout Homework 1</a>
+                        <a href="https://github.com/krishnagupta71/cmpe272/" class="card-link" target="_blank">Github</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Homework 2</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Sept 25, 2021</h6>
+                        <p class="card-text">Using your newly obtained web hosting solution. Create a company web site.</p>
+                        <a href="homework2/" class="card-link">Checkout Homework 2</a>
+                        <a href="https://github.com/krishnagupta71/cmpe272/" class="card-link" target="_blank">Github</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Homework 3</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Oct 2, 2021</h6>
+                        <p class="card-text">Secure a section of your website that can only be accessed by admin. Securing Users tab</p>
+                        <a href="homework3/" class="card-link">Checkout Homework 3</a>
+                        <a href="https://github.com/krishnagupta71/cmpe272/" class="card-link" target="_blank">Github</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Homework 4</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Oct 9, 2021</h6>
+                        <p class="card-text">Create a mySQL database for your company web site</p>
+                        <a href="homework4/" class="card-link">Checkout Homework 4</a>
+                        <a href="https://github.com/krishnagupta71/cmpe272/" class="card-link" target="_blank">Github</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Homework 5</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Oct 23, 2021</h6>
+                        <p class="card-text">Tracking with web cookies - Show last 5 visited products</p>
+                        <a href="homework5/" class="card-link">Checkout Homework 5</a>
+                        <a href="https://github.com/krishnagupta71/cmpe272/" class="card-link" target="_blank">Github</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Homework 6</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Nov 6, 2021</h6>
+                        <p class="card-text">Make use of curl to read user list from friend's website</p>
+                        <a href="homework6/" class="card-link">Checkout Homework 6</a>
+                        <a href="https://github.com/krishnagupta71/cmpe272/" class="card-link" target="_blank">Github</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
